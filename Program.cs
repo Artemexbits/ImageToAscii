2737edf23003675df2d92829e1de513f00d14c64
Artemexbits <=>
2023-03-19 16:45:29 +0100

    added simple color differentiation

--- Program.cs
+++ Program.cs
@@ -10,45 +10,107 @@ class Program
 {
     static void Main(string[] args)
     {
-        const int size = 150;
-        const int quality = 75;
-        string filepath = System.AppContext.BaseDirectory + "NewCaledonianCrow.jpg";
-    
+        string filepath = System.AppContext.BaseDirectory + "test2.jpg";
+        byte[,] arr = new byte[0, 0];
         Console.WriteLine("PATH: " + filepath);
         try {
         using (var image = new Bitmap(System.Drawing.Image.FromFile(filepath)))
         {
-            int width, height;
-            if (image.Width > image.Height)
-            {
-                width = size;
-                height = Convert.ToInt32(image.Height * size / (double)image.Width);
-            }
-            else
-            {
-                width = Convert.ToInt32(image.Width * size / (double)image.Height);
-                height = size;
-            }
-            var resized = new Bitmap(width, height);
-            using (var graphics = Graphics.FromImage(resized))
-            {
-                graphics.CompositingQuality = CompositingQuality.HighSpeed;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.DrawImage(image, 0, 0, width, height);
-                using (var output = File.Open("newImage.jpg", FileMode.Create))
-                {
-                    var qualityParamId = Encoder.Quality;
-                    var encoderParameters = new EncoderParameters(1);
-                    encoderParameters.Param[0] = new EncoderParameter(qualityParamId, quality);
-                    var codec = ImageCodecInfo.GetImageDecoders()
-                        .FirstOrDefault(codec => codec.FormatID == ImageFormat.Jpeg.Guid);
-                    resized.Save(output, codec, encoderParameters);
+            int width = image.Width;
+            int height = image.Height;
+
+            arr = new byte[height, width];
+
+            for(int i = 0; i < height; i++) {
+                for(int j = 0; j < width; j++) {
+                    //Console.WriteLine("-------------------");
+                    var argb = image.GetPixel(j, i);
+                    byte r = (byte) (argb.ToArgb() >> 16 & 255);
+                    byte g = (byte) (argb.ToArgb() >> 8 & 255);
+                    byte b = (byte) (argb.ToArgb() & 255);
+                    // Console.WriteLine("r: " + r);
+                    // Console.WriteLine("g: " + g);
+                    // Console.WriteLine("b: " + b);
+                    // Console.WriteLine("argb: " + argb.ToString());
+                    // Console.WriteLine("-------------------");
+                    if(ablack(r,g,b, 10)) {
+                        arr[i, j] = (int)'#';
+                    } else
+                    if(ared(r,g,b, 10)) {
+                        arr[i, j] = (int)'Z';
+                    } else 
+                    if(agreen(r,g,b, 10)) {
+                        arr[i, j] = (int)'V';
+                    } else
+                    if(apurple(r,g,b, 10)) {
+                        arr[i, j] = (int)'X';
+                    } else {
+                        arr[i, j] = (int)' ';
+                    }
+                    
                 }
             }
         }
         } catch (Exception e) {
             Console.WriteLine("ERROR: " + e);
+        }
+        try {
+            using(FileStream fs = new FileStream("convertedTest2.txt", FileMode.Create, FileAccess.Write)) {
+                for(int i = 0; i < arr.GetLength(0); i++) {
+                    for(int j = 0; j < arr.GetLength(1); j++) {
+                        fs.WriteByte(arr[i, j]);
+                    }
+                    fs.WriteByte(10);
+                }
+                fs.Flush();
+            }
+        } catch (Exception e) {
+            Console.WriteLine("ERROR: " + e);
+        }
+    }
+
+    private static bool awhite(byte r, byte g, byte b) {
+        return false;
+    }
+    private static bool ablack(int r, int g, int b, int d) {
+        if(r < d*2 && g < d*2 && b < d*2) {
+            //Console.WriteLine("black");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private static bool ared(int r, int g, int b, int d) {
+        if(Enumerable.Range(((255-d) < 0 ? 0 : (255-d)), 255).Contains(r) && g < d*2 && b < d*2) {
+            //Console.WriteLine("red");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private static bool agreen(int r, int g, int b, int d) {
+        if(Enumerable.Range(((255-d) < 0 ? 0 : (255-d)), 255).Contains(g) && r < d*2 && b < d*2) {
+            //Console.WriteLine("green");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private static bool ablue(int r, int g, int b, int d) {
+        if(Enumerable.Range(((255-d) < 0 ? 0 : (255-d)), 255).Contains(b) && g < d*2 && r < d*2) {
+            //Console.WriteLine("blue");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private static bool apurple(int r, int g, int b, int d) {
+        if(Enumerable.Range(((255-d) < 0 ? 0 : (255-d)), 255).Contains(b) && g < d*2
+        && Enumerable.Range(((128-d) < 0 ? 0 : (128-d)), (128+d)).Contains(r)) {
+            //Console.WriteLine("purple");
+            return true;
+        } else {
+            return false;
         }
     }
 }
