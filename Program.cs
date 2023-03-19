8679d488bd717799b6e8d27d9a146616f44dc5a2
Artemexbits <=>
2023-03-19 16:45:31 +0100

    image resizing test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Drawing;
+﻿using System;
+using System.IO;
+using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.Linq;
 
 namespace ImageToAscii;
 class Program
